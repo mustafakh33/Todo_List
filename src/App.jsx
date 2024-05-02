34f951a70c4e67fe1b9888fa3ebcac19eeaf02be
Mustafa Khaled <146621425+mustafakh33@users.noreply.github.com>
2024-05-02 12:11:14 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoListContainer from "./components/todoListContainer/TodoListContainer";
+
+const App = () => {
+  return (
+    <div className="container">
+      <TodoListContainer />
+    </div>
+  );
+};
+
+export default App;
